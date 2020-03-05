72763206c2606a9853829cc22c9715889a4a2fe3
ZoroZero <40843946+ZoroZero@users.noreply.github.com>
2020-03-05 16:44:33 +0700

    add function to dungeon class
    + Add init, update, render function to dungeon class

--- src/states/game/PlayState.lua
+++ src/states/game/PlayState.lua
@@ -3,7 +3,11 @@ PlayState = Class {__includes = BaseState}
 
 -- INIT
 function PlayState:init()
-    self.room = Room(1);
+    self.player = 1;
+
+    self.dungeon = Dungeon(self.player);
+
+    self.current_Room = Room(self.player);
     
 end
 
@@ -12,12 +16,12 @@ function PlayState:update(dt)
     -- if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then 
     --     game_State_Machine:change('play');
     -- end
-    self.room:update(dt);
+    self.dungeon:update(dt);
 end
 
 -- RENDER
 function PlayState:render()
-    self.room:render();
+    self.dungeon:render();
     
     love.graphics.printf(tostring(MAP_WIDTH) .. ' ' .. tostring(MAP_HEIGHT), 0, 0, VIRTUAL_WIDTH, 'center');
 end
--- src/world/Dungeon.lua
+++ src/world/Dungeon.lua
@@ -11,16 +11,36 @@ function Dungeon:init(player)
     -- current room
     self.current_Room = Room(player);
 
+    -- next room
+    self.next_Room = nil;
 
+    -- Camera
+    self.camera_X = 0;
+    self.camera_Y = 0;
+    
+    -- If shifting room
+    self.shifting = false;
 
 end
 
 -- UPDATE
 function Dungeon:update(dt)
-
+    if not self.shifting then
+        self.current_Room:update(dt)
+    else
+        -- Still update player animation 
+    end
 end
 
 -- RENDER
 function Dungeon:render()
+    if self.shifting then 
+        love.graphics.translate(-self.camera_X, - self.camera_Y);
+    end
 
+    self.current_Room:render();
+
+    if self.next_Room then 
+        self.next_Room:render();
+    end
 end
